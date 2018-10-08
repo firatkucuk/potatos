6adc0408c87b8930dae0dc825edee1f0b803758d
Peter Buijs <peter.buijs4045@gmail.com>
2018-10-08 23:35:24 +0200

    Use auto-advancing character output (TTY mode)

--- potatos.asm
+++ potatos.asm
@@ -25,23 +25,13 @@ print_string:                            ; Routine: output string in SI to scree
     mov bl, 01h                          ; first color in the palette
 
 .repeat
-    mov ah, 09h                          ; int10 / 09 write character at cursor position
-    mov cx, 01h
-
     lodsb                                ; Get character from string (source segment)
     cmp al, 0
     je .done                             ; If char is zero, end of string
 
-    mov ah, 09h                          ; int 10h 'print char' function
+    mov ah, 0Eh                          ; int 10h 'print char' function TTY mode
     add bl, 01h                          ; change color
     int 10h                              ; Otherwise, print it
-
-    mov ah, 03h                          ; int10 / 03 get cursor position
-    int 10h
-
-    mov ah, 02h                          ; int10 / 02 set cursor position
-    add dl, 01h                          ; incrementing column 1
-    int 10h
 
     jmp .repeat
 
